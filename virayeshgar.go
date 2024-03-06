4f4b41fad2596ebafaff9b613aa50e40b671effa
Amirali Esfandiari <amiralinull@gmail.com>
2024-03-06 16:35:31 +0330

    add python syntax

--- virayeshgar.go
+++ virayeshgar.go
@@ -245,6 +245,34 @@ var HLDB = []*EditorSyntax{
 		mce:   "--]]",
 		flags: HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS,
 	},
+	{
+		filetype:  "python",
+		filematch: []string{".py"},
+		keywords: []string{
+			"as", "assert", "break", "class", "continue", "def", "del",
+			"elif", "else", "except", "finally", "for", "from", "global",
+			"if", "import", "in", "lambda", "nonlocal", "pass", "raise",
+			"return", "try", "while", "with", "yield",
+
+			"and|", "False|", "is|", "None|", "not|", "or|", "True|",
+			"int|", "float|", "bool|", "str|", "abs|", "all|", "any|",
+			"ascii|", "bin|", "bytearray|", "bytes|", "callable|", "chr|",
+			"classmethod|", "compile|", "complex|", "delattr|", "dict|",
+			"dir|", "divmod|", "enumerate|", "eval|", "exec|", "filter|",
+			"format|", "frozenset|", "getattr|", "globals|", "hasattr|",
+			"hash|", "help|", "hex|", "id|", "input|", "isinstance|",
+			"issubclass|", "iter|", "len|", "list|", "locals|", "map|",
+			"max|", "memoryview|", "min|", "next|", "object|", "oct|",
+			"open|", "ord|", "pow|", "print|", "property|", "range|",
+			"repr|", "reversed|", "round|", "set|", "setattr|", "slice|",
+			"sorted|", "staticmethod|", "sum|", "super|", "tuple|", "type|",
+			"vars|", "zip|",
+		},
+		scs:   "#",
+		mcs:   "",
+		mce:   "",
+		flags: HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS,
+	},
 }
 
 type Row struct {
